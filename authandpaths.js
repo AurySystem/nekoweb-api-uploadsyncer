e5b74bbb44cdd98c42104718e7e717fd9375806d
AurySystem <22009843+AurySystem@users.noreply.github.com>
2025-03-30 23:06:07 -0400

    setup script

--- authandpaths.js
+++
@@ -1,9 +0,0 @@
-const auth = " "; // your api token here
-const source = "./production/"; // source folder for incoming .mds
-const cacheSources = "./.production/"; // cache for comparing on startup
-
-module.exports = {
-  auth,
-  source,
-  cacheSources
-};
---
+++ setup.js
@@ -0,0 +1,53 @@
+
+const fs = require("fs");
+
+let auth = "a";
+let sources = "b";
+let cache = "d";
+
+let temp = `const auth = "${auth}"; // your api token here
+const source = "${sources}"; // source folder for incoming .mds
+const cacheSources = "${cache}"; // cache for comparing on startup
+
+module.exports = {
+  auth,
+  source,
+  cacheSources
+};`
+
+
+const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout
+  });
+  readline.question(`Enter API key: `, en => {
+    console.log(`Confirmed`);
+    auth = en;
+    
+    readline.question(`Enter md source directory: `, en => {
+      console.log(`Confirmed`);
+      sources = en;
+      readline.question(`Enter caching directory: `, en => {
+        console.log(`Finalizing`);
+        cache = en;
+temp = `const auth = "${auth}"; // your api token here
+const source = "${sources}"; // source folder for incoming .mds
+const cacheSources = "${cache}"; // cache for comparing on startup
+
+module.exports = {
+  auth,
+  source,
+  cacheSources
+};`
+        console.log(temp);
+        readline.close();
+        fs.promises.writeFile('./authandpaths.js', temp).catch(er => {
+          if (er) {
+            console.error(er);
+          }
+        });
+      });      
+    });
+  });
+
+  
